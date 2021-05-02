9a08b3d789959817e16f508b77ea01d91310e88e
bunny232 <bunnyracer23@yahoo.com>
2021-05-02 03:48:00 -0400

    Update settings.py

--- wgt_site/settings.py
+++ wgt_site/settings.py
@@ -83,7 +83,9 @@ WSGI_APPLICATION = 'wgt_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'WakeGolfTour.db',
+        'NAME': os.path.join(BASE_DIR, 'WakeGolfTour.db')
+       
+
     }
 }
 
