4c1fa795f0df10384607f01c634779a4e65f30ea
bunny232 <bunnyracer23@yahoo.com>
2021-05-07 06:09:17 -0400

    Update settings.py

--- wgt_site/settings.py
+++ wgt_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [wakegolftourfinal-wakegolftourfinal.apps-crc.testing]
 
 
 # Application definition
